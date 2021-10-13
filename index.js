8acd75ccbf8ba9bbe3cf9d3fd4caedf4be2bbce7
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-13 15:25:27 +0000

    map solution

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 /*
 Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    return nums.map(el => Math.pow(el,2)).sort((a,b) => a - b)
+};
