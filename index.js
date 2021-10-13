0fe07cd3f1dc258126ed3f51bb8752ccea76fcc0
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-13 15:42:24 +0000

    two pointer solution

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+977 Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 */
 
 /**
@@ -7,5 +7,17 @@ Given an integer array nums sorted in non-decreasing order, return an array of t
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    return nums.map(el => Math.pow(el,2)).sort((a,b) => a - b)
+  let squaredArray = new Array(nums.length).fill(0)
+  let left = 0;
+  let right = nums.length - 1
+  for (let i = squaredArray.length - 1; i >= 0; i--) {
+    if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+      squaredArray[i] = nums[right] ** 2
+      right--
+    } else {
+      squaredArray[i] = nums[left] ** 2
+      left++
+    }
+  }
+  return squaredArray
 };
